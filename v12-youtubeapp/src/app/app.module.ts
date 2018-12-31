727f4cb2f4ac0d1fc500fdf94e7b277bdf70546c
Harry Alvarado <harry2alvarado8@gmail.com>
2018-12-31 14:50:11 -0500

    Creando componentes (navbar, home) para la aplicación de YouTubeApp.

--- v12-youtubeapp/src/app/app.module.ts
+++ v12-youtubeapp/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// services
+import { YoutubeService } from './services/youtube.service';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    YoutubeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
