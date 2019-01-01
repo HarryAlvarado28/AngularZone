115abe8a802d369a7c0ea5ddde15fa1a0a91d534
Harry Alvarado <harry2alvarado8@gmail.com>
2019-01-01 04:24:39 -0500

    Se configura el archivo carga-service para subir imagenes a FireStore.

--- v13-fotosfirebase/src/app/services/carga-imagenes.service.ts
+++ v13-fotosfirebase/src/app/services/carga-imagenes.service.ts
@@ -13,6 +13,43 @@ export class CargaImagenesService {
   constructor(private db: AngularFirestore) { }
 
   cargarImagenesFirebase(imagenes: FileItem[]){
+    const storageRef = firebase.storage().ref();
+    for(const item of imagenes){
+      item.estaSubiendo = true;
+      if(item.progreso >= 100){
+        continue;
+      }
+
+      const uploadTask: firebase.storage.UploadTask =
+          storageRef.child(`${this.CARPETA_IMAGENES}/${item.nombreArchivo}`)
+            .put(item.archivo);
+
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: firebase.storage.UploadTaskSnapshot) =>
+                  item.progreso = (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+          (error) => console.error('Error al subir',error),
+          () => {
+            console.log('Imagen cargando correctamente');
+            // item.url = uploadTask.snapshot.downloadURL;
+            // item.estaSubiendo = false;
+            // this.guardarImange({
+            //   nombre: item.nombreArchivo,
+            //   url: item.url
+            // })
+
+            uploadTask.snapshot.ref.getDownloadURL().then( (downloadURL) => {
+                          console.log('File available at', downloadURL);
+                          item.url = downloadURL;
+                          item.estaSubiendo = false;
+                          // Guardamos los datos de la imagen en la BD
+                          this.guardarImange({
+                            nombre: item.nombreArchivo,
+                            url: item.url
+                          });
+                        });
+
+          });
+    }
     console.log(imagenes);
   }
 
