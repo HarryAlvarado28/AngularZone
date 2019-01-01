eee3de62477746f3ae61e21a9a57d48b111006c6
Harry Alvarado <harry2alvarado8@gmail.com>
2019-01-01 04:36:04 -0500

    Agregamo metodo getCollactions para llamar las imagenes que estan en el FireStorage.

--- v13-fotosfirebase/src/app/components/fotos/fotos.component.ts
+++ v13-fotosfirebase/src/app/components/fotos/fotos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item { nombre: string; url: string;}
 
 @Component({
   selector: 'app-fotos',
@@ -7,7 +11,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FotosComponent implements OnInit {
 
-  constructor() { }
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+
+  constructor(private afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<Item>('img');
+    this.items = this.itemsCollection.valueChanges();
+  }
 
   ngOnInit() {
   }
