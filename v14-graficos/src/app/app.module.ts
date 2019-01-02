6bdc64808ae5333da744e526d93f5006d38a3818
Harry Alvarado <harry2alvarado8@gmail.com>
2019-01-02 17:04:48 -0500

    Implementando grafica de lineas.

--- v14-graficos/src/app/app.module.ts
+++ v14-graficos/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { LineaComponent } from './components/linea/linea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
